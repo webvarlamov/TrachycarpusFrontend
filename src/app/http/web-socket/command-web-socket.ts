640d0c1373f65a2f7905375ab011792c4b68517b
varlamov <varlamov@nemosoft.ru>
2021-06-04 18:12:13 +0300

    RTC Data Chanel

--- src/app/http/web-socket/command-web-socket.ts
+++ src/app/http/web-socket/command-web-socket.ts
@@ -23,10 +23,16 @@ export enum CommandWebSocketMessageType {
   set_offer_from_remote = "set_offer_from_remote",
   set_answer_from_remote = "set_answer_from_remote",
   set_candidate_from_remote = "set_candidate_from_remote",
-  update_user_device_web_socket_sessions = "update_user_device_web_socket_sessions",
+
   received_the_call = "received_the_call",
   received_the_answer = "received_the_answer",
   received_candidate_from_remote = "received_candidate_from_remote",
+
+  receive_data_channel_offer = "receive_data_channel_offer",
+  receive_data_channel_answer = "receive_data_channel_answer",
+  receive_data_channel_candidate = "receive_data_channel_candidate",
+
+  update_user_device_web_socket_sessions = "update_user_device_web_socket_sessions",
   alert = "alert",
 
   CLIENT_ERROR = "CLIENT_ERROR"
